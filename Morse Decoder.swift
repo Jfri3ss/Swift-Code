7e8b5a922558442a896c0621a2ffe15039cd9268
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-03-23 20:34:29 -0400

    Add files via upload
    Morse Decoder.swift Project

---
+++ Morse Decoder.swift
@@ -0,0 +1,108 @@
+//
+//  Morse Decoder.swift
+//  CodeCademy Code
+//
+//  Created by Steve Jobs on 3/23/24.
+//
+
+import Foundation
+import SwiftUI
+
+var englishText = "this is a secret message"
+
+var secretMessage = ".... --- .-- -.. -.--   .--. .- .-. - -. . .-."
+
+var letterToMorse: [String: String] = [
+  "a": "·−",
+  "b": "−···",
+  "c": "−·−·",
+  "d": "−··",
+  "e": "·",
+  "f": "··−·",
+  "g": "−−·",
+  "h": "····",
+  "i": "··",
+  "j": "·−−−",
+  "k": "−·−",
+  "l": "·−··",
+  "m": "−−",
+  "n": "−·",
+  "o": "−−−",
+  "p": "·−−·",
+  "q": "−−·−",
+  "r": "·−·",
+  "s": "···",
+  "t": "−",
+  "u": "··−",
+  "v": "···−",
+  "w": "·−−",
+  "x": "−··−",
+  "y": "−·−−",
+  "z": "−−··"
+]
+
+//print(letterToMorse)
+
+//Store encrypted message here
+var morseText = ""
+var decodedMessage = ""
+//Remember to use correct Array Syntax
+var morseCodeArray = [String]()
+var currMorse = ""
+
+for element in englishText {
+if let morseChar = letterToMorse["\(element)"] {
+  morseText += morseChar + " "
+  } else {
+  morseText += "   "
+  }
+}
+//print(morseText)
+
+for char in secretMessage {
+  if char != " " {
+    currMorse.append(char)
+  } else {
+  switch currMorse {
+    case "":
+      currMorse += " "
+    case " ":
+      morseCodeArray.append(" ")
+      currMorse = ""
+    default:
+      morseCodeArray.append(currMorse)
+      currMorse = ""
+    }
+  }
+}
+
+morseCodeArray.append(currMorse)
+//print(morseCodeArray)
+
+
+var morseToLetter: [String: String] = [:]
+
+//Review you mixed up the order of your key and values in (letter, morseChar)
+for (letter, morseChar) in letterToMorse {
+  morseToLetter[morseChar] = letter
+}
+
+for morseValue in morseCodeArray {
+  if let letterChar = morseToLetter[morseValue] {
+    decodedMessage += letterChar
+  } else {
+    decodedMessage += " "
+  }
+}
+
+print(decodedMessage)
+
+
+
+
+
+
+
+
+
+
