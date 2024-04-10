893e6512883eb4b33f9f44151c7764b4e2e3e074
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-04-10 11:50:46 -0400

    Update SwiftBank.swift

--- SwiftBank.swift
+++ SwiftBank.swift
@@ -1,11 +1,15 @@
+// Write your code below 🏦
+
 struct SwiftBank { 
 private let password: String 
 
-init(password: String) { 
+init(password: String, initialDeposit: Double) { 
   self.password = password
+  makeDeposit(ofAmount: initialDeposit)
 }
 
-//Function
+
+//Function also known as a helper method
 private func isValid(enteredPassword: String) -> Bool { 
     if enteredPassword == password { 
       return true 
@@ -18,22 +22,15 @@ private var balance: Double = 0
 
 static let depositBonusRate = 0.01
 
+private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
+    var newDepositValue = (deposit *  0.01)
+        return newDepositValue
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  mutating func makeDeposit(ofAmount depositAmount: Double) {
+    finalDepositWithBonus(fromInitialDeposit: depositAmount)
+      let depositWithBonus = depositAmount
+      print("Making a deposit of \(depositAmount) with a bonus rate. The final amount deposited is \(depositWithBonus)")
+      balance = depositWithBonus
+  }
+}
