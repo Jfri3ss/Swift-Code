3a1c7ca9b6f9bcd0d077d7372767ea46dcc916c8
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-04-11 12:06:20 -0400

    Update SwiftBank.swift

--- SwiftBank.swift
+++ SwiftBank.swift
@@ -1,15 +1,13 @@
-// Write your code below 🏦
-
 struct SwiftBank { 
 private let password: String 
 
 init(password: String, initialDeposit: Double) { 
-  self.password = password
+  self.password = "Deezz Nuts"
+  //Function call inside init method, after password is initalized
   makeDeposit(ofAmount: initialDeposit)
 }
 
-
-//Function also known as a helper method
+//Function 1 also known as a helper method, checks for valid password (validator function)
 private func isValid(enteredPassword: String) -> Bool { 
     if enteredPassword == password { 
       return true 
@@ -18,19 +16,69 @@ private func isValid(enteredPassword: String) -> Bool {
   }
 }
 
-private var balance: Double = 0 
+private var balance: Double = 0 { 
+  //property observer
+didSet {
+  if balance < 100 { 
+  displayLowBalanceMessage()
+  }
+  }
+}
 
 static let depositBonusRate = 0.01
 
+//Function 2 finalDepositWithBonus
 private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
-    var newDepositValue = (deposit *  0.01)
-        return newDepositValue
+  var depositPlusBonus = ((deposit * SwiftBank.depositBonusRate) + deposit)
+  return deposit
 }
 
-  mutating func makeDeposit(ofAmount depositAmount: Double) {
-    finalDepositWithBonus(fromInitialDeposit: depositAmount)
-      let depositWithBonus = depositAmount
-      print("Making a deposit of \(depositAmount) with a bonus rate. The final amount deposited is \(depositWithBonus)")
+
+//Function 3 makeDeposit
+  mutating func makeDeposit(ofAmount deposit: Double) {
+    finalDepositWithBonus(fromInitialDeposit: deposit)
+      let depositWithBonus = deposit
+      print("Making a deposit of \(deposit) with a bonus rate. The final amount deposited is \(depositWithBonus)")
       balance = depositWithBonus
+}
+
+
+//Function 4 displayBalance
+  func displayBalance(usingPassword password: String) { 
+    var isValidUser = isValid(enteredPassword: password) 
+    if isValidUser == false { 
+        print("Error: Invalid password. Cannot retrieve balance.")
+    } else { 
+        print("Your current balance is \(balance)")
+    }
+}
+
+//Function 5 makeWithdrawal
+func makeWithdrawal(ofAmount withdrawalAmount: Double, usingPassword password: String) { 
+    var isValidUser = isValid(enteredPassword: password)
+    if isValidUser == false {
+      print("Error: Invalid password. Cannot make withdrawal.")
+    } else { 
+      print("Making a $\(withdrawalAmount) withdrawal")
+    }
+  }
+
+//Function 6 displayLowBalanceMessage
+private func displayLowBalanceMessage() { 
+print("Alert: Your balance is under $100")
   }
 }
+
+var myAccount = SwiftBank(password: "Deezz Nuts", initialDeposit: 500)
+
+//myAccount = SwiftBank(password: "Deezz Nuts", initialDeposit: 50)
+
+
+
+
+
+
+
+
+
+
