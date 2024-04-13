3bb981a7a801222e4facf878af0133a8942ec890
Jeremiah Fries <77308708+Jfri3ss@users.noreply.github.com>
2024-04-12 20:23:07 -0400

    Update Pokedex.swift

--- Pokedex.swift
+++ Pokedex.swift
@@ -1,3 +1,5 @@
+// Write your code below 🤾‍♂️ 
+
 class Pokemon { 
 var num = 0 
 var name = ""
@@ -5,15 +7,58 @@ var type = [""]
 var ability = [""]
 
 
-init(num: Int, name: String, type: [String], ability: String) {
-self.num = num 
-self.name = name 
-self.type = type 
-self.ability = ability
+init(num: Int, name: String, type: [String], ability: [String]) {
+  self.num = num 
+  self.name = name 
+  self.type = type 
+  self.ability = ability
+  
+}
+
+func displayInfo() { 
+
+print("No.            #\(num)")
+print("Name           \(name)")
+print("Type           \(type)")
+print("Abilities   \(ability)")
+print("==========================================")
   }
 }
 
+var bulbasaur = Pokemon(num: 1, name: "Bulbasaur", type: ["Grass 🌱", "Poison 💀"], ability: ["Overgrow"])
 
+var charmander = Pokemon(num: 4, name: "Charmander", type: ["Fire 🔥"], ability: ["Blaze"])
+
+var squirtle = Pokemon(num: 7, name: "Squirtle", type: ["Water 💧"], ability: ["Torrent"])
+
+class GigantamaxPokemon: Pokemon { 
+  var location = "" 
+
+  override func displayInfo() { 
+
+  print("No.            #\(num)")
+  print("Name           \(name)")
+  print("Type           \(type)")
+  print("Abilities   \(location)")
+  print("==========================================")
+}
+
+  init(num: Int, name: String, type: [String], ability: [String], location: String) {
+
+    super.init(num: num, name: name, type: type, ability: ability) 
+    self.location = location
+    }
+
+
+}
+
+bulbasaur.displayInfo()
+charmander.displayInfo()
+squirtle.displayInfo()
+GigantamaxPokemon.displayInfo()
+
+
+var charizard = GigantamaxPokemon(num: 6, name: "Charizard", type: ["Fire 🔥"], ability: ["Blaze"], location: "Lake of Outrage")
 
 
 
